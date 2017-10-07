381262c39b39c34cec2d3387f30c3673e47747e6
Asma Sadia <a.sadia51299@gmail.com>
2017-10-07 20:58:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians,instruments){
+
+
+}
+function johnLennonFacts(){
+  
+}
+  
+function iLoveTheBeatles(7){
+ var array= new Array();
+  do {
+    array.push("I love the Beatles")
+  
+} while (let i = 0; i < 7; i++);
+  array.push("I love the Beatles")
+}
