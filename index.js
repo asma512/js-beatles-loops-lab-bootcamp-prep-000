b25ddc0e8c593e760a2e16f32ec12a6c20602a54
Asma Sadia <a.sadia51299@gmail.com>
2017-10-07 21:01:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function johnLennonFacts(){
 }
 
 function iLoveTheBeatles(7){
+ var array = new Array();
   for (let i = 0; i < 7+1; i++) {
    array.push("I love the Beatles!")
-
   }
+  return array
+}
